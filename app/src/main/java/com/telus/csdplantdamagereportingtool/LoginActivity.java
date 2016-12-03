40bd2be52fd67ad1639a330f6948dfade2ec3f9f
Chad <yittrix@gmail.com>
2016-12-03 15:33:23 -0700

    Camera Integration

--- app/src/main/java/com/telus/csdplantdamagereportingtool/LoginActivity.java
+++ app/src/main/java/com/telus/csdplantdamagereportingtool/LoginActivity.java
@@ -3,6 +3,7 @@ package com.telus.csdplantdamagereportingtool;
 import android.animation.Animator;
 import android.animation.AnimatorListenerAdapter;
 import android.annotation.TargetApi;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.support.annotation.NonNull;
 import android.support.design.widget.Snackbar;
@@ -86,6 +87,8 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
             @Override
             public void onClick(View view) {
                 attemptLogin();
+                Intent myIntent = new Intent(view.getContext(), mainScreen.class);
+                startActivityForResult(myIntent, 0);
             }
         });
 
