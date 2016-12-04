73bbffe0a3353e9c05eff468dd08004456b93bb2
Chad <yittrix@gmail.com>
2016-12-03 19:32:43 -0700

    DB integration

--- app/src/main/java/com/telus/csdplantdamagereportingtool/dBHelper.java
+++ app/src/main/java/com/telus/csdplantdamagereportingtool/dBHelper.java
@@ -1,6 +1,8 @@
 package com.telus.csdplantdamagereportingtool;
 
+import android.content.ContentValues;
 import android.content.Context;
+import android.database.ContentObservable;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
@@ -25,7 +27,7 @@ public class dBHelper extends SQLiteOpenHelper {
 
     public dBHelper(Context context) {
         super(context, DATABASE_NAME, null, 1);
-        SQLiteDatabase db = this.getWritableDatabase();
+        //SQLiteDatabase db = this.getWritableDatabase();
     }
 
     @Override
@@ -49,5 +51,24 @@ public class dBHelper extends SQLiteOpenHelper {
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
         db.execSQL("DROP TABLE IF EXISTS " + TABLE_NAME);
         onCreate(db);
+    }
+
+    public boolean insertData(String Tid, String datetime, int region, int CLLI, int damageType, String lat, String lng, String text, String picLocation) {
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues contentValues = new ContentValues();
+        contentValues.put(COL_2, Tid);
+        contentValues.put(COL_3, datetime);
+        contentValues.put(COL_4, region);
+        contentValues.put(COL_5, CLLI);
+        contentValues.put(COL_6, damageType);
+        contentValues.put(COL_7, lat);
+        contentValues.put(COL_8, lng);
+        contentValues.put(COL_9, text);
+        contentValues.put(COL_10, picLocation);
+        long result = db.insert(TABLE_NAME, null, contentValues);
+        if (result == -1)
+            return false;
+        else
+            return true;
     }
 }
